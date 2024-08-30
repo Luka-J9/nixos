ebe98067700334d09dd6a222b3636e5336127a67
Luka-J9@users.noreply.github.com <Luka-J9@users.noreply.github.com>
2024-08-29 20:45:56 -0400

    update to add default applications

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -1,5 +1,27 @@
 { config, pkgs, ... }:
 
+
+let 
+  autostartPrograms = [ pkgs.vesktop pkgs.signal-desktop pkgs._1password-gui];
+  autoStartConfigs = builtins.listToAttrs (map
+      (pkg:
+        {
+          name = ".config/autostart/" + pkg.pname + ".desktop";
+          value =
+            if pkg ? desktopItem then {
+              # Application has a desktopItem entry. 
+              # Assume that it was made with makeDesktopEntry, which exposes a
+              # text attribute with the contents of the .desktop file
+              text = pkg.desktopItem.text;
+            } else {
+              # Application does *not* have a desktopItem entry. Try to find a
+              # matching .desktop name in /share/apaplications
+              source = (pkg + "/share/applications/" + pkg.pname + ".desktop");
+            };
+        })
+      autostartPrograms);
+
+in
 {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
@@ -16,9 +38,49 @@
     ../../modules/home-manager/3dPrinting.nix
   ];
 
-  home.file.".face" = {
-    source = ../icons/profile.png;
-  };
+
+  home.packages = with pkgs; [
+      signal-desktop
+      zed-editor
+      _1password-gui
+      vesktop
+      ungoogled-chromium
+      kitty
+      prusa-slicer
+      ollama
+
+    cantarell-fonts
+    hack-font
+    inter
+    jetbrains-mono
+    liberation_ttf
+    monaspace
+    noto-fonts
+    ubuntu_font_family
+    (nerdfonts.override {
+      fonts = [
+        "FiraCode"
+        "DroidSansMono"
+        "JetBrainsMono"
+      ];
+    })
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # pkgs.hello
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
 
   _1password-git = {
     enable = true;
@@ -95,55 +157,11 @@
   # release notes.
   home.stateVersion = "24.05"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = with pkgs; [
-    cantarell-fonts
-    hack-font
-    inter
-    jetbrains-mono
-    liberation_ttf
-    monaspace
-    noto-fonts
-    ubuntu_font_family
-    (nerdfonts.override {
-      fonts = [
-        "FiraCode"
-        "DroidSansMono"
-        "JetBrainsMono"
-      ];
-    })
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-  ];
-
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+  home.file =  autoStartConfigs // {".face" = {
+    source = ../icons/profile.png;
+  };
   };
 
   # Home Manager can also manage your environment variables through
