e1e452c6552d234bbd07b714a81bd75bb8eb5277
Luka-J9@users.noreply.github.com <Luka-J9@users.noreply.github.com>
2024-09-05 21:44:28 -0400

    enabel quiet boot

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -38,7 +38,7 @@
   ];
 
   # Bootloader.
-  quiet-boot.enable = false;
+  quiet-boot.enable = true;
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
