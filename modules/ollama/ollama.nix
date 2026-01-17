a8f7033c3feb715733b179d986b16a5e76785ee6
Luka-J9 <development@lukaj9.com>
2026-01-16 22:53:30 -0500

    update

--- modules/ollama/ollama.nix
+++ modules/ollama/ollama.nix
@@ -9,7 +9,7 @@
   config = {
     services.ollama = {
       enable = true;
-      package = pkgs.ollama-rocm;
+      package = pkgs.ollama-vulkan;
       environmentVariables = {
         HSA_OVERRIDE_GFX_VERSION = "11.5.0"; # Native for Radeon 8060S (Strix Halo)
         HSA_ENABLE_SDMA = "0";
